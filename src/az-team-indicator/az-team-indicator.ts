8893b0755e20a3856147d9bac030137780ea0e18
Jan Hrdina <jan.hrdka@gmail.com>
2016-06-11 00:34:31 +0200

    TeamIndicator wave animation

--- src/az-team-indicator/az-team-indicator.ts
+++ src/az-team-indicator/az-team-indicator.ts
@@ -1,10 +1,40 @@
 /// <reference path="../../bower_components/polymer-ts/polymer-ts.d.ts"/>
 
 @component('az-team-indicator')
+@behavior(Polymer['NeonAnimationRunnerBehavior'])
 class AzTeamIndicator extends polymer.Base {
+
+  // From NeonAnimationRunnerBehavior:
+  playAnimation: () => void;
+
+  //=================================================================
 
   @property({ type: String, value: 'teamA', reflectToAttribute: true })
   public team: string;
+
+  @property({ type: Boolean, value: false })
+  public noWave: boolean;
+
+  @property({ type: Object, value: function() {
+    return {
+      name: 'wave-animation',
+      node: this.$.wave,
+      timing: {
+        "duration": 1500,
+        "easing": 'cubic-bezier(0.4, 0, 0.2, 1)',
+        "fill": 'both',
+        delay: 200
+      }
+    };
+  }})
+  public animationConfig: any;
+
+  @observe('team')
+  private _teamChanged(team): void {
+    if (!this.noWave) {
+      this.playAnimation();
+    }
+  }
 }
 
 AzTeamIndicator.register();
